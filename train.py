7acf861b41245279e52d17e5eef5a6412e2da900
vltanh <vltheanh030698@gmail.com>
2020-05-01 10:11:21 +0700

    reproducibility and dataloader getter

--- train.py
+++ train.py
@@ -7,8 +7,8 @@ from tqdm import tqdm
 from torchnet import meter
 
 from workers.trainer import Trainer
-from utils.random_seed import set_seed
-from utils.getter import get_instance
+from utils.random_seed import set_seed, set_determinism
+from utils.getter import get_instance, get_data
 
 import argparse
 import pprint
@@ -19,6 +19,7 @@ def train(config):
 
     pprint.PrettyPrinter(indent=2).pprint(config)
 
+    # Get device
     dev_id = 'cuda:{}'.format(config['gpus']) \
         if torch.cuda.is_available() and config.get('gpus', None) is not None \
         else 'cpu'
@@ -34,38 +35,11 @@ def train(config):
             config[item] = pretrained["config"][item]
 
     # 1: Load datasets
-    if config['dataset'].get('train', None) is not None and \
-       config['dataset'].get('val', None) is not None:
-        set_seed()
-        train_dataset = get_instance(config['dataset']['train'])
-        train_dataloader = get_instance(config['dataset']['train']['loader'],
-                                        dataset=train_dataset)
-
-        set_seed()
-        val_dataset = get_instance(config['dataset']['val'])
-        val_dataloader = get_instance(config['dataset']['val']['loader'],
-                                      dataset=val_dataset)
-    elif config['dataset'].get('trainval'):
-        set_seed()
-        dataset_cfg = config['dataset']['trainval']
-        ratio = dataset_cfg['ratio']
-        dataset = get_instance(dataset_cfg)
-        train_sz = int(ratio * len(dataset))
-        val_sz = len(dataset) - train_sz
-        train_dataset, val_dataset = random_split(dataset, [train_sz, val_sz])
-
-        set_seed()
-        train_dataloader = get_instance(config['dataset']['trainval']['train_loader'],
-                                        dataset=train_dataset)
-
-        set_seed()
-        val_dataloader = get_instance(config['dataset']['trainval']['val_loader'],
-                                      dataset=val_dataset)
-    else:
-        raise Exception('Dataset config is not correctly formatted.')
+    train_dataloader, val_dataloader = \
+        get_data(config['dataset'], config['seed'])
 
     # 2: Define network
-    set_seed()
+    set_seed(config['seed'])
     model = get_instance(config['model']).to(device)
 
     # Train from pretrained if it is not None
@@ -73,28 +47,28 @@ def train(config):
         model.load_state_dict(pretrained['model_state_dict'])
 
     # 3: Define loss
-    set_seed()
+    set_seed(config['seed'])
     criterion = get_instance(config['loss']).to(device)
 
     # 4: Define Optimizer
-    set_seed()
+    set_seed(config['seed'])
     optimizer = get_instance(config['optimizer'],
                              params=model.parameters())
     if pretrained is not None:
         optimizer.load_state_dict(pretrained['optimizer_state_dict'])
 
     # 5: Define Scheduler
-    set_seed()
+    set_seed(config['seed'])
     scheduler = get_instance(config['scheduler'],
                              optimizer=optimizer)
 
     # 6: Define metrics
-    set_seed()
+    set_seed(config['seed'])
     metric = {mcfg['name']: get_instance(mcfg)
               for mcfg in config['metric']}
 
     # 6: Create trainer
-    set_seed()
+    set_seed(config['seed'])
     trainer = Trainer(device=device,
                       config=config,
                       model=model,
@@ -104,7 +78,7 @@ def train(config):
                       metric=metric)
 
     # 7: Start to train
-    set_seed()
+    set_seed(config['seed'])
     trainer.train(train_dataloader=train_dataloader,
                   val_dataloader=val_dataloader)
 
@@ -122,4 +96,5 @@ if __name__ == "__main__":
     config['gpus'] = args.gpus
     config['debug'] = args.debug
 
+    set_determinism()
     train(config)
