a6ce86259958d5143aebfbefc53524494a02b785
vltanh <vltheanh030698@gmail.com>
2020-06-22 21:07:22 +0700

    modify resnet version

--- models/extractors/resnet.py
+++ models/extractors/resnet.py
@@ -6,17 +6,17 @@ from .extractor_network import ExtractorNetwork
 
 class ResNetExtractor(ExtractorNetwork):
     arch = {
-        18: models.resnet18,
-        34: models.resnet34,
-        50: models.resnet50,
-        101: models.resnet101,
-        152: models.resnet152,
+        'resnet18': models.resnet18,
+        'resnet34': models.resnet34,
+        'resnet50': models.resnet50,
+        'resnet101': models.resnet101,
+        'resnet152': models.resnet152,
     }
 
     def __init__(self, version):
         super().__init__()
-        assert version in [18, 34, 50, 101, 152], \
-            'ResNet{version} is not implemented.'
+        assert version in ResNetExtractor.arch, \
+            f'{version} is not implemented.'
         cnn = ResNetExtractor.arch[version](pretrained=True)
         self.extractor = nn.Sequential(*list(cnn.children())[:-1])
         self.feature_dim = cnn.fc.in_features
