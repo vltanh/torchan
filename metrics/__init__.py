a459025d30c2f046d55bf34516e78833d8570949
vltanh <vltheanh030698@gmail.com>
2020-06-22 21:04:26 +0700

    add some (1) classification metric(s)

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,3 +1,2 @@
-from .segmentation.mean_iou import MeanIoU
-from .segmentation.pixel_accuracy import PixelAccuracy
-from .segmentation.dice_score import DiceScore
+from .segmentation import *
+from .classification import *
--- metrics/segmentation/mean_iou.py
+++ metrics/segmentation/mean_iou.py
@@ -1,39 +1,75 @@
 import torch
+from torch import nn
 import torch.nn.functional as F
 
 from utils.segmentation import multi_class_prediction, binary_prediction
 
 
 class MeanIoU():
-    def __init__(self, nclasses, ignore_index=None, eps=1e-6):
+    def __init__(self, nclasses, ignore_index=None, eps=1e-9):
         super().__init__()
         assert nclasses > 0
 
         self.nclasses = nclasses
-        self.pred_fn = multi_class_prediction
-        if nclasses == 1:
-            self.nclasses += 1
-            self.pred_fn = binary_prediction
         self.ignore_index = ignore_index
         self.eps = eps
         self.reset()
 
     def calculate(self, output, target):
-        batch_size = output.size(0)
-        ious = torch.zeros(self.nclasses, batch_size)
+        nclasses = output.size(1)
+        prediction = torch.argmax(output, dim=1)
+        prediction = F.one_hot(prediction, nclasses).bool()
+        target = F.one_hot(target, nclasses).bool()
+        intersection = (prediction & target).sum((-3, -2))
+        union = (prediction | target).sum((-3, -2))
+        return intersection.cpu(), union.cpu()
 
-        prediction = self.pred_fn(output)
+    def update(self, value):
+        self.intersection += value[0].sum(0)
+        self.union += value[1].sum(0)
+        self.sample_size += value[0].size(0)
+        self.summary()
 
+    def value(self):
+        ious = (self.intersection + self.eps) / (self.union + self.eps)
+        miou = ious.sum()
+        nclasses = ious.size(0)
         if self.ignore_index is not None:
-            target_mask = (target == self.ignore_index).bool()
-            prediction[target_mask] = self.ignore_index
+            miou -= ious[self.ignore_index]
+            nclasses -= 1
+        return miou / nclasses
 
-        prediction = F.one_hot(prediction, self.nclasses).bool()
-        target = F.one_hot(target, self.nclasses).bool()
+    def reset(self):
+        self.intersection = torch.zeros(self.nclasses).float()
+        self.union = torch.zeros(self.nclasses).float()
+        self.sample_size = 0
+
+    def summary(self):
+        class_iou = (self.intersection + self.eps) / (self.union + self.eps)
+
+        print(f'mIoU: {self.value():.6f}')
+        for i, x in enumerate(class_iou):
+            print(f'\tClass {i:3d}: {x:.6f}')
+
+
+class _MeanIoU():
+    def __init__(self, nclasses, ignore_index=None, eps=1e-9):
+        super().__init__()
+        assert nclasses > 0
+
+        self.nclasses = nclasses
+        self.ignore_index = ignore_index
+        self.eps = eps
+        self.reset()
+
+    def calculate(self, output, target):
+        nclasses = output.size(1)
+        prediction = torch.argmax(output, dim=1)
+        prediction = F.one_hot(prediction, nclasses).bool()
+        target = F.one_hot(target, nclasses).bool()
         intersection = (prediction & target).sum((-3, -2))
         union = (prediction | target).sum((-3, -2))
         ious = (intersection.float() + self.eps) / (union.float() + self.eps)
-
         return ious.cpu()
 
     def update(self, value):
@@ -41,7 +77,13 @@ class MeanIoU():
         self.sample_size += value.size(0)
 
     def value(self):
-        return (self.mean_class / self.sample_size).mean()
+        ious = self.mean_class
+        miou = ious.sum() / self.sample_size
+        nclasses = ious.size(0)
+        if self.ignore_index is not None:
+            miou -= ious[self.ignore_index] / self.sample_size
+            nclasses -= 1
+        return miou / nclasses
 
     def reset(self):
         self.mean_class = torch.zeros(self.nclasses).float()
@@ -53,3 +95,8 @@ class MeanIoU():
         print(f'mIoU: {self.value():.6f}')
         for i, x in enumerate(class_iou):
             print(f'\tClass {i:3d}: {x:.6f}')
+
+
+class ModifiedMeanIoU(MeanIoU):
+    def calculate(self, output, target):
+        return super().calculate(output[-1], target[-1])
