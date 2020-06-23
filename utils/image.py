49b7de7a47364ad12a9bcca23ad5637f9eb71a0d
vltanh <vltheanh030698@gmail.com>
2020-06-23 14:16:15 +0700

    delete unnecessary files

--- utils/image.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-from torchvision import transforms
-
-
-def load_image_as_tensor(img_path):
-    img = Image.open(img_path)
-    img = transforms.ToTensor()(img)
-    return img
